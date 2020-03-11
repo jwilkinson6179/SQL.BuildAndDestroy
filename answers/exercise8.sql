aee95cd4fc15e5c96a5e7ba9b8c61761e9e0814c
James Wilkinson <jwilkinson@James-Wilkinsons-MacBook-Pro.local>
2020-03-11 13:31:22 -0400

    10 of 20

---
+++ answers/exercise8.sql
@@ -0,0 +1,12 @@
+CREATE TABLE Students (student_name VARCHAR(255),
+    address VARCHAR(255),
+    city VARCHAR(255),
+    post_code VARCHAR(5),
+    country VARCHAR(150));
+INSERT INTO Students (
+    student_name,
+    address,
+    city,
+    post_code,
+    country)
+    VALUES ('Jane Doe', '57 Union St', 'Glasgow', 'G13RB', 'Scotland');
